a231efefbfc2fc724680f459a8970958d175ee19
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2026-05-22 02:38:27 +0530

    Write a C program to check whether a triangle is equilateral, isosceles, or scalene using a function with parameter and return type.

---
+++ Triangle_WithParameterWithReturnType.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int Triangle(int s1,int s2,int s3)
+{
+	if(s1==s2&&s2==s3)
+		{
+			return 1;
+		}
+	else if(s1==s2||s2==s3||s1==s3)
+		{
+		   return 0;
+		}
+		else
+		{
+			return -1;
+		}
+	}
+void main()
+{
+	int ans;
+	int s1,s2,s3;
+	
+	printf("Enter three sides of triangle: ");
+    scanf("%d%d%d", &s1,&s2,&s3);
+    
+    ans=Triangle(s1,s2,s3);
+    
+    
+	if(ans == 1)
+	{
+		printf("Triangle is Equilateral");
+	}
+	else if(ans == 0)
+	{
+		printf("Triangle is Isosceles");
+	}
+	else
+	{
+		printf("Triangle is Scalene");
+	}
+
+	return 0;
+}
