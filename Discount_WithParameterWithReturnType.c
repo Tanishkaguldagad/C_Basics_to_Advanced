1447573cd1b1d7a28b00419bcf6e8e8c97b23fb2
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2026-05-22 03:17:34 +0530

    Write a C program to calculate discount and final amount based on student status using a function with parameter and return type.

---
+++ Discount_WithParameterWithReturnType.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+
+float Discount(float price, char student)
+{
+    float discount, finalAmount;
+
+    if(student == 'y' || student == 'Y')
+    {
+        if(price > 500)
+            discount = price * 0.20;
+        else
+            discount = price * 0.10;
+    }
+    else
+    {
+        if(price > 600)
+            discount = price * 0.15;
+        else
+            discount = 0;
+    }
+
+    finalAmount = price - discount;
+
+    return finalAmount;
+}
+
+int main()
+{
+    float price, ans;
+    char student;
+
+    printf("Enter total price: ");
+    scanf("%f", &price);
+
+    printf("Are you a student? (y/n): ");
+    scanf(" %c", &student);
+
+    ans = Discount(price, student);
+
+    printf("Final Amount to Pay: %.2f", ans);
+
+    return 0;
+}
