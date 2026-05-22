b3b20545fc4d6502e8899a324da8d1ffc0cd886e
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2026-05-23 00:56:59 +0530

    Write a C program using with parameter without return type function to check whether a number is even or odd.

---
+++ EvenOdd_WithParameterWithoutReturnType.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void evenodd(int a)
+{	
+	if(a%2==0)
+	{
+		printf("Number is even");
+	}
+	else
+	{
+		printf("Number is odd");
+	}
+}
+    	
+	int main()
+	{
+	   evenodd(5);
+	   return 0;
+}
