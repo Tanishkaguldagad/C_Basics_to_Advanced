bb0bb45b179db630e411035392096a1955bbcf49
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2026-05-23 02:06:15 +0530

    Write a C program to calculate total salary using DA, TA, and HRA with parameter with return type function.

--- TotalSalary_WithParameterWithReturnType.c
+++ TotalSalary_WithParameterWithReturnType.c
@@ -1,39 +1,37 @@
 #include<stdio.h>
 
-void TotalSalary(float Basic)
+float TotalSalary(float Basic)
 {
-	float da, ta, hra, total;
+    float da, ta, hra, total;
 
-	if(Basic <= 5000)
-	{
-		da = Basic * 0.10;
+    if(Basic <= 5000)
+    {
+        da = Basic * 0.10;
         ta = Basic * 0.20;
         hra = Basic * 0.25;
-	}
-	else
-	{
-		da = Basic * 0.15;
+    }
+    else
+    {
+        da = Basic * 0.15;
         ta = Basic * 0.25;
         hra = Basic * 0.30;
-	}
+    }
 
-	total = Basic + da + ta + hra;
+    total = Basic + da + ta + hra;
 
-    printf("Basic Salary = %.2f\n", Basic);
-    printf("DA = %.2f\n", da);
-    printf("TA = %.2f\n", ta);
-    printf("HRA = %.2f\n", hra);
-    printf("Total Salary = %.2f\n", total);
+    return total;
 }
 
 int main()
 {
-	float salary;
+    float salary, ans;
 
-	printf("Enter Basic Salary: ");
-	scanf("%f", &salary);
+    printf("Enter Basic Salary: ");
+    scanf("%f", &salary);
 
-	TotalSalary(salary);
+    ans = TotalSalary(salary);
 
-	return 0;
+    printf("Total Salary = %.2f", ans);
+
+    return 0;
 }
