e3c220f2d882653b0ed20e7d29080a15f92f29cd
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2026-05-24 03:40:33 +0530

    Write a C program to check whether a number is palindrome or not using without parameter with return type function.

---
+++ Palindrome_WithoutParameterWithReturnType.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int Palindrome()
+{
+	int num;
+	int r1,r2,r3,rev;
+	int q1;
+	
+	r1=num%10;
+	q1=num/10;
+	
+	r2=q1%10;
+	r3=q1/10;
+
+	rev=r1*100+r2*10+r3;
+	
+	if(num==rev)
+	{
+		return 1;
+	}
+	else{
+	    return 0;
+	}
+}
+int main()
+{
+	int ans;
+	ans=Palindrome(121);
+	
+	if(ans==1)
+	{
+		printf("Num is Palindrome");
+	}
+	else{
+		printf("Num is not Palindrome");
+	}
+	return 0;
+}
