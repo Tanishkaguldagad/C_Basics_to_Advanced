6967363c48d6aa084b1a30146e8dc9d99deee8f2
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2026-05-25 02:19:11 +0530

    Write a C program to classify a person into child, teenager, adult, or senior category using without parameter without return type function.

---
+++ Age_WithoutParameterWithoutReturnType.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+void AgeCategory()
+{
+	int age;
+	
+	printf("Enter age: ");
+    scanf("%d", &age);
+    
+    if(age < 12)
+    {
+        printf("Person is child");
+    }
+    else if(age >= 12 && age <= 19)
+    {
+        printf("Person is Teenager");
+    }
+    else if(age >= 20 && age <= 59)
+    {
+        printf("Person is Adult");
+    }
+    else
+    {
+        printf("Person is Senior");
+    }
+}
+
+int main()
+{
+    AgeCategory();
+    return 0;
+}
