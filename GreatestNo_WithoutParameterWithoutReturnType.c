ff197114a4a1bf2362a1c2729b5c43614512daec
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2026-05-30 01:45:51 +0530

    Write a C program to find the greatest among three numbers using a Without Parameter Without Return Type function.

---
+++ GreatestNo_WithoutParameterWithoutReturnType.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+void GreatestNo()
+{
+	int a,b,c;
+	
+	printf("Enter three numbers: ");
+    scanf("%d%d%d", &a, &b, &c);
+	
+    if(a > b)
+    {
+        if(a > c)
+        {
+            printf("Greatest Number = %d", a);
+        }
+        else
+        {
+           printf("Greatest Number = %d", c);
+        }
+    }
+    else
+    {
+        if(b > c)
+        {
+           printf("Greatest Number = %d", b);
+        }
+        else
+        {
+            printf("Greatest Number = %d", c);
+        }
+    }
+}
+int main()
+{
+	int a,b,c;
+	
+    GreatestNo();
+
+    return 0;
+}
