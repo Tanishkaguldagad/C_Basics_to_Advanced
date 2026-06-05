0b08d49621149fe6a8ddaa6dd6744cfda39dcf4f
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2026-06-06 03:01:28 +0530

    Write a C program to print all Prime Numbers from 1 to n using a function without parameter and with return type.

---
+++ PrimeNoUsingLoops_WithoutParameterWithReturnType.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int PrimeSeries()
+{
+    int n;
+
+    printf("Enter a number: ");
+    scanf("%d",&n);
+
+    for(int i=2;i<=n;i++)
+    {
+        int isPrime=1;
+
+        for(int j=2;j<i;j++)
+        {
+            if(i%j==0)
+            {
+                isPrime=0;
+                break;
+            }
+        }
+
+        if(isPrime==1)
+            printf("%d ",i);
+    }
+
+    return 1;
+}
+
+int main()
+{
+    PrimeSeries();
+    return 0;
+}
