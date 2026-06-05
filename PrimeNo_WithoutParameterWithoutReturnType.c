11c5a493b59c0653913ccb1ede6f4dfafaf3e724
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2026-06-06 02:15:29 +0530

    Write a C program to check whether a given number is Prime or Not Prime using a function without parameter and without return type.

---
+++ PrimeNo_WithoutParameterWithoutReturnType.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<math.h>
+
+void PrimeNo()
+{
+	int n;
+    int i, flag = 0;
+    
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    if(n <= 1)
+    {
+        printf("Number is not Prime");
+        return;
+    }
+
+    for(i = 2; i <= sqrt(n); i++)
+    {
+        if(n % i == 0)
+        {
+            flag = 1;
+            break;
+        }
+    }
+
+    if(flag == 0)
+        printf("Number is Prime");
+    else
+        printf("Number is not Prime");
+}
+
+int main()
+{
+    PrimeNo();
+
+    return 0;
+}
