571a564af8a9a459c8c46d99dd9985ba37e10b63
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2026-06-06 20:39:20 +0530

    Write a C program to check whether a given number is a Strong Number or not using a function without parameter and without return type.

---
+++ StrongNo_WithoutParameterWithoutReturnType.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void Strong()
+{
+    int n, temp, digit;
+    int sum = 0;
+    int factorial;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    temp = n;
+
+    while(n > 0)
+    {
+        digit = n % 10;
+        factorial = 1;
+
+        for(int i = 1; i <= digit; i++)
+        {
+            factorial = factorial * i;
+        }
+
+        sum = sum + factorial;
+        n = n / 10;
+    }
+
+    if(sum == temp)
+        printf("Strong Number");
+    else
+        printf("Not Strong Number");
+}
+
+int main()
+{
+    Strong();
+    return 0;
+}
