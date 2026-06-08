55e135415454e3af2bf48c8d9b32a5453955f68a
Tanishkaguldagad <162014132+Tanishkaguldagad@users.noreply.github.com>
2026-06-09 00:18:34 +0530

    Write a C program to print all Strong Numbers from 1 to N using a function with parameter and without return type.

---
+++ StrongNoByUsingLoops_WithParameterWithoutReturnType.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void StrongNo(int n)
+{
+    for(int num = 1; num <= n; num++)
+    {
+        int original = num;
+        int sum = 0, digit, fact;
+
+        while(original > 0)
+        {
+            digit = original % 10;
+            fact = 1;
+
+            for(int i = 1; i <= digit; i++)
+            {
+                fact = fact * i;
+            }
+
+            sum = sum + fact;
+            original = original / 10;
+        }
+
+        if(sum == num)
+        {
+            printf("%d ", num);
+        }
+    }
+}
+
+void main()
+{
+    int n;
+
+    printf("Enter value of n: ");
+    scanf("%d", &n);
+
+    StrongNo(n);
+}
